85d50193b7410dcd02c7792c74c9c434837a2749
Fernando Ortiz <fernandodev.ortiz@gmail.com>
2021-08-05 17:05:02 -0600

    Trying to fix the firebase issue

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -2,12 +2,12 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 
 var firebaseConfig = {
-   apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
-   authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
-   projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
-   storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
-   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
-   appId: process.env.REACT_APP_FIREBASE_APPID
+   apiKey: "AIzaSyCX1LQcO_PuLEVKItsOHZVsvFAY7wr6r3M",
+   authDomain: "frontendmentor-app-crowfunding.firebaseapp.com",
+   projectId: "frontendmentor-app-crowfunding",
+   storageBucket: "frontendmentor-app-crowfunding.appspot.com",
+   messagingSenderId: "596011208329",
+   appId: "1:596011208329:web:9b21a5addce27c94160058"
 };
 
 // Initialize Firebase
